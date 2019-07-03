75d0f19b367d650b29d5048c8ad89a6f2ea86485
Khofidin <offiedz@gmail.com>
2019-07-03 20:11:03 +0700

    Add Pager and Tabs

---
+++ app/src/main/java/in/khofid/lajartantjap/view/MovieFragment.kt
@@ -0,0 +1,21 @@
+package `in`.khofid.lajartantjap.view
+
+
+import `in`.khofid.lajartantjap.R
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MovieFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movie, container, false)
+    }
+
+
+}
