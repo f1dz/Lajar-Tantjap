e7d03303de3ff9024cb96b54799e411a1d6e4272
Khofidin <offiedz@gmail.com>
2019-07-04 09:56:32 +0700

    Add RecyclerView fro Movies

--- app/src/main/java/in/khofid/lajartantjap/model/Movie.kt
+++ app/src/main/java/in/khofid/lajartantjap/model/Movie.kt
@@ -8,7 +8,7 @@ data class Movie(
     var id: Int? = null,
     var title: String? = null,
     var overview: String? = null,
-    var vote_average: Double? = null,
+    var vote_average: Float,
     var poster_path: String? = null,
     var backdrop_path: String? = null,
     var release_date: String? = null
--- app/src/main/java/in/khofid/lajartantjap/view/MovieFragment.kt
+++ app/src/main/java/in/khofid/lajartantjap/view/MovieFragment.kt
@@ -2,26 +2,38 @@ package `in`.khofid.lajartantjap.view
 
 
 import `in`.khofid.lajartantjap.R
+import `in`.khofid.lajartantjap.adapter.MovieAdapter
 import `in`.khofid.lajartantjap.model.Movie
 import `in`.khofid.lajartantjap.presenter.MoviePresenter
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_movie.view.*
 
 class MovieFragment : Fragment(), MovieView {
 
     private var movies: MutableList<Movie> = arrayListOf()
     private lateinit var rootView: View
     private lateinit var presenter: MoviePresenter
+    private lateinit var adapter: MovieAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         rootView = inflater.inflate(R.layout.fragment_movie, container, false)
+
+        adapter = MovieAdapter(rootView.context, movies){
+            Toast.makeText(rootView.context, it.title, Toast.LENGTH_SHORT).show()
+        }
+
+        rootView.rv_movies.layoutManager = LinearLayoutManager(activity)
+        rootView.rv_movies.adapter = adapter
+
 
         presenter = MoviePresenter(this)
         presenter.getMovieList()
@@ -38,7 +50,9 @@ class MovieFragment : Fragment(), MovieView {
     }
 
     override fun loadMovies(data: List<Movie>) {
+        movies.clear()
         movies.addAll(data)
+        adapter.notifyDataSetChanged()
     }
 
     override fun movieNotFound() {
