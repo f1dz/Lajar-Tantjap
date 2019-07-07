d8ea3c4832815e6a4a54b90f234ce1d50e8811ba
Khofidin <offiedz@gmail.com>
2019-07-07 19:19:18 +0700

    Add bottom navigation bar

--- app/src/main/java/in/khofid/lajartantjap/view/main/MainActivity.kt
+++ app/src/main/java/in/khofid/lajartantjap/view/main/MainActivity.kt
@@ -1,13 +1,16 @@
 package `in`.khofid.lajartantjap.view.main
 
 import `in`.khofid.lajartantjap.R
-import `in`.khofid.lajartantjap.adapter.CustomPagerAdapter
+import `in`.khofid.lajartantjap.view.movie.MovieFragment
+import `in`.khofid.lajartantjap.view.tv.TvShowFragment
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,9 +19,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        view_pager.adapter = CustomPagerAdapter(supportFragmentManager, this)
-        tabs.setupWithViewPager(view_pager)
-
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.movies -> loadFragment(MovieFragment())
+                R.id.tvShow -> loadFragment(TvShowFragment())
+                R.id.favorites -> {
+                    Toast.makeText(this, "Favorites", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
+        bottom_navigation.selectedItemId = R.id.movies
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -33,5 +44,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, fragment, fragment::class.java.simpleName)
+            .commit()
     }
 }
